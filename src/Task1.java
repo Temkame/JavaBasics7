76b069826a1c594b1fb779edcee10bd98fdb156d
temkame <temkame@yandex.ru>
2022-03-27 22:34:14 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,15 @@
+/*
+Метод finalize класса Cat
+
+В классе Cat создать метод protected void finalize() throws Throwable
+ */
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
+class Cat {
+    protected void finalize() throws Throwable {
+
+    }
+}
